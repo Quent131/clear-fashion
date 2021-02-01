d93fee5c12d6328d258115a0338048854b78585b
BOURGUE Quentin <quentin.bourgue@edu.devinci.fr>
2021-02-01 20:02:28 +0100

    feat : can crape all 3 brands

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sources/dedicatedbrand');
+const mudjeansbrand = require('./sources/mudjeansbrand');
+const adressebrand = require('./sources/adressebrand');
 
 async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    const products = await adressebrand.scrape(eshop);
 
     console.log(products);
     console.log('done');
@@ -18,4 +20,8 @@ async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
 
 const [,, eshop] = process.argv;
 
-sandbox(eshop);
+//sandbox(eshop);
+const URLMJ = 'https://mudjeans.eu/collections/men';
+//sandbox(URLMJ);
+const URLadresse = 'https://adresse.paris/602-nouveautes';
+sandbox(URLadresse);
